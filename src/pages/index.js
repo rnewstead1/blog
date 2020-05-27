07eb383b5f17843182a850e48a4ae310589ad42b
Rachel Normand <racheljnewstead@gmail.com>
2020-05-27 17:27:12 +0100

    home tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,20 @@ const IndexPage = ({
         <div className="left-panel" style={{ width: '65%' }}>
           <div style={{ marginBottom: '1rem' }}>
             <img src={pentlands} alt="Pentland Hills" />
-            <p>Happiest in the mountains.</p>
-            <p>I'm currently moving my blog here. Head to my <a href="https://powderach-running.blogspot.com/">old blog</a> for older posts!</p>
+            <div style={{ marginBottom: '1rem', width: '80%', marginLeft: 'auto', marginRight: 'auto', padding: '1rem' }}>
+              <i>Hill repeats & recover, <a href="https://linktr.ee/runwildandbepoetry" style={{ textDecoration: 'none', color: '#000' }}>Sydney Zester</a></i><br/><br/>
+              <div style={{ textAlign: 'right' }}>
+                some days we bundle up<br/>
+                and run hill repeats outside<br/>
+                in the hollow winter air.<br/>
+                other days we wrap up cozy<br/>
+                and slurp mugs of coffee<br/>
+                while snug on the sofa.<br/>
+                i think it’s best to do both.<br/>
+              </div>
+            </div>
+            <hr />
+            <p>I'm moving my blog here. For older posts go <a href="https://powderach-running.blogspot.com/">here</a>.</p>
           </div>
           <div className="blog-post-container">
             <div className="blog-post">
