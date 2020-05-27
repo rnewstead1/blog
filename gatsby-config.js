e1df744eceffba0c125918aea98b4e5061653493
Rachel Normand <racheljnewstead@gmail.com>
2020-05-27 17:46:43 +0100

    linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,65 +1,65 @@
 module.exports = {
   siteMetadata: {
-    title: `Powderach Running`,
-    description: `Happiest in the mountains`,
-    author: `@powderach`,
-    siteUrl: `https://powderach.com`
+    title: 'Powderach Running',
+    description: 'Happiest in the mountains',
+    author: '@powderach',
+    siteUrl: 'https://powderach.com',
   },
   plugins: [
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
-        excerpt_separator: `<!-- end -->`,
+        excerpt_separator: '<!-- end -->',
         plugins: [
           {
-            resolve: "gatsby-remark-embed-video",
+            resolve: 'gatsby-remark-embed-video',
             options: {
               width: 800,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
-              related: true, //Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              ratio: 1.77,
+              height: 400,
+              related: true,
+              noIframeBorder: true,
               urlOverrides: [
                 {
                   id: 'youtube',
                   embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
-                }
-              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-              containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
-            }
+                },
+              ],
+              containerClass: 'embedVideo-container',
+            },
           },
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 600,
               showCaptions: ['title'],
               markdownCaptions: true,
               backgroundColor: 'none',
-            }
+            },
           },
-        ]
-      }
+        ],
+      },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `markdown-pages`,
+        name: 'markdown-pages',
         path: `${__dirname}/src/posts`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-twitter`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-twitter',
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: 'gatsby-plugin-feed',
       options: {
         query: `
           {
@@ -75,17 +75,15 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map((edge) => ({
+                ...edge.node.frontmatter,
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                custom_elements: [{ 'content:encoded': edge.node.html }],
+              })),
             query: `
               {
                 allMarkdownRemark(
@@ -105,11 +103,11 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title: "Powderach.com RSS Feed",
+            output: '/rss.xml',
+            title: 'Powderach.com RSS Feed',
           },
         ],
       },
     },
   ],
-}
+};
