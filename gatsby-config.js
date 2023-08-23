f6483e940de24b25e53859739d48257702f56325
Rachel Normand <racheljnewstead@gmail.com>
2023-08-23 22:21:08 +0100

    remove link to broken rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,56 +57,5 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) =>
-              allMarkdownRemark.edges.map((edge) => ({
-                ...edge.node.frontmatter,
-                description: edge.node.excerpt,
-                date: edge.node.frontmatter.date,
-                url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                custom_elements: [{ 'content:encoded': edge.node.html }],
-              })),
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      frontmatter {
-                        title
-                        date
-                        path
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: 'Powderach.com RSS Feed',
-          },
-        ],
-      },
-    },
   ],
 };
